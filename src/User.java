1c11807c41de0b4837d41d9506fd2facf3a0cf87
Amy Huang <ahuang@ucvts.org>
2019-11-13 20:38:58 -0500

    first

--- src/User.java
+++ src/User.java
@@ -8,7 +8,18 @@ public class User {
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
     //                                                                        //
     ////////////////////////////////////////////////////////////////////////////
-    
+    public User(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
     /**
      * Formats the first and last name in preparation to be written to the data file.
      * 
